2f41c834e079d54edba172fcd9351ef4bc435b52
8JotaScript8 <jotagebe07@gmail.com>
2026-02-13 17:26:54 -0300

    13/02/2026

--- escola/settings.py
+++ escola/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')y*ovk!@mrulc#$hk*vetv-2)02*!eoij#4+i-vdp&-b!pvq8='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
